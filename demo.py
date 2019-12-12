e7f49c6a7e88912f999675c132508ecc61e4d015
2392863668 <2392863668@qq.com>
2019-12-12 11:41:31 +0800

    Revert "load bullet picture"
    This reverts commit 663d14cbb2bf055c247d1311daa075e4f817a674.

--- demo.py
+++ demo.py
@@ -7,18 +7,12 @@ import pygame
 import sys
 
 pygame.init()  # 初始化pygame
-size = width, height = 640, 480  # 设置窗口大小
+size = width, height = 320, 240  # 设置窗口大小
 screen = pygame.display.set_mode(size)  # 显示窗口
-color = (0, 0, 0)  # 设置颜色
-ball = pygame.image.load('bullet.png')  # 加载图片
-ballrect = ball.get_rect()  # 获取矩形区域
 
 while True:  # 死循环确保窗口一直显示
     for event in pygame.event.get():  # 遍历所有事件
         if event.type == pygame.QUIT:  # 如果单击关闭窗口，则退出
             sys.exit()
-    screen.fill(color)  # 填充颜色(设置为0，执不执行这行代码都一样)
-    screen.blit(ball, ballrect)  # 将图片画到窗口上
-    pygame.display.flip()  # 更新全部显示
 
 pygame.quit()  # 退出pygame
